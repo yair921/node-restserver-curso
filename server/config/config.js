160169c56ef1dad57b065cd09460bac7b326792c
Yair <yair_918@hotmail.com>
2018-06-05 21:58:18 -0500

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
   urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-  urlDB = 'mongodb://cafe-user:abc123@ds017726.mlab.com:17726/cafe';
+  urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
 // mongodb://localhost:27017/cafe
