eabcb46eae92142509ac3dce92c79c1f943c612d
Yair <yair_918@hotmail.com>
2018-06-05 21:30:07 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,23 @@
 // ===========================
-// puerto
+// Puerto
 // ===========================
-process.env.PORT =  process.env.PORT || 3000;
+process.env.PORT = process.env.PORT || 3000;
+
+// ===========================
+// Entorno
+// ===========================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ===========================
+// Base de datos
+// ===========================
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+  urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+  urlDB = 'mongodb://cafe-user:abc123@ds017726.mlab.com:17726/cafe';
+}
+process.env.URLDB = urlDB;
+// mongodb://localhost:27017/cafe
+// mongodb://<dbuser>:<dbpassword>@ds017726.mlab.com:17726/cafe
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,33 +1,112 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const _ = require('underscore');
+const Usuario = require('./../models/usuarios');
 const app = express();
 
 app.get('/usuario', function (req, res) {
-  res.json('Get Usuario!');
+  let desde = req.query.desde || 0;
+
+  desde = Number(desde);
+
+  let limite = req.query.limite || 5;
+  limite = Number(limite);
+
+  Usuario.find({ estado: true }, 'nombre email role estado google')
+    .skip(desde)
+    .limit(limite)
+    .exec((err, usuarios) => {
+      if (err) {
+        res.status(400).json({
+          ok: false,
+          err
+        });
+        return;
+      }
+      Usuario.count({ estado: true }, (err, conteo) => {
+        res.json({
+          ok: true,
+          usuarios,
+          cuantos: conteo
+        });
+      });
+
+    });
 });
 
 app.post('/usuario', function (req, res) {
   let body = req.body;
-  if (body.nombre === undefined) {
-    res.status(400).json({
-      ok: false,
-      mensaje: 'El nombre es necesario'
-    })
-  } else {
+
+
+  let usuario = new Usuario({
+    nombre: body.nombre,
+    email: body.email,
+    password: bcrypt.hashSync(body.password, 10),
+    role: body.role
+  });
+
+
+  usuario.save((err, usuarioDB) => {
+    if (err) {
+      res.status(400).json({
+        ok: false,
+        err
+      });
+      return;
+    }
     res.json({
-      persona: body
+      ok: true,
+      usuario: usuarioDB
     });
-  }
+  })
+
 });
 
 app.put('/usuario/:id', function (req, res) {
   let id = req.params.id;
-  res.json({
-    id
+  let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+
+
+  Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
+    if (err) {
+      return res.status(400).json({
+        ok: false,
+        err
+      });
+    }
+    res.json({
+      ok: true,
+      usuario: usuarioDB
+    });
   });
+
 });
 
-app.delete('/usuario', function (req, res) {
-  res.json('Get Usuario!');
+app.delete('/usuario/:id', function (req, res) {
+  let id = req.params.id;
+  let cambiaEstado = {
+    estado: false
+  }
+  Usuario.findByIdAndUpdate(id, cambiaEstado, { new: true }, (err, usuarioBorrado) => {
+    if (err) {
+      return res.status(400).json({
+        ok: false,
+        err
+      });
+    }
+    if (!usuarioBorrado) {
+      return res.status(400).json({
+        ok: false,
+        err: {
+          message: 'Usuario no encontrado!'
+        }
+      });
+    }
+    res.json({
+      ok: true,
+      usuario: usuarioBorrado
+    })
+  });
 });
 
 module.exports = app;
